e500a27d7abc5ce7e5fe3141f266ed865fd05c92
Migorithm <saka90030@gmail.com>
2021-07-14 00:13:24 +0900

    Recursion

---
+++ Algorithm_49_parenthesis.py
@@ -0,0 +1,32 @@
+"""
+https://programmers.co.kr/learn/courses/30/lessons/60058
+"""
+
+from collections import deque
+def solution(p):
+    #base case
+    if p =="":
+        return ""
+    u = ""
+    v = ""
+    for i in range(len(p)):
+        u += p[i]
+        if u.count("(") == u.count(")"):
+            v += p[i+1:]
+            break
+    # recursive case
+    if u.startswith("("):
+        return u + solution(v)
+    else:
+        ru = "(" + solution(v) + ")"
+        repl = u[1:-1].replace("(",".")
+        repl2 = repl.replace(")","(")
+        repl3 = repl2.replace(".",")")
+        return ru + repl3
+
+
+
+
+
+print(solution(")))(()())((("))
+
